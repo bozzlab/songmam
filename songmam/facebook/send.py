ee5d78deb0f11c058350faee67aaa0c930a737e8
Nutchanon Ninyawee <me@nutchanon.org>
2020-07-16 17:12:06 +0700

    add send response

--- songmam/facebook/send.py
+++ songmam/facebook/send.py
@@ -1,1 +1,9 @@
 # https://developers.facebook.com/docs/messenger-platform/send-messages#send_api_basics
+from pydantic import BaseModel
+from typing  import Optional
+
+
+
+class SendResponse(BaseModel):
+    recipient_id: Optional[str]
+    message_id: str
