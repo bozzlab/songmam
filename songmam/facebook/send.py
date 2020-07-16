141708a8cb61a206db4d46788847e395b9eb6df4
Nutchanon Ninyawee <me@nutchanon.org>
2020-07-16 17:28:16 +0700

    create msgtag

--- songmam/facebook/send.py
+++ songmam/facebook/send.py
@@ -1,7 +1,18 @@
 # https://developers.facebook.com/docs/messenger-platform/send-messages#send_api_basics
-from pydantic import BaseModel
-from typing  import Optional
+from enum import auto
 
+from pydantic import BaseModel
+from typing import Optional
+
+from songmam.utils import AutoName
+
+
+class MessageTag(AutoName):
+    """https://developers.facebook.com/docs/messenger-platform/send-messages/message-tags"""
+    CONFIRMED_EVENT_UPDATE = auto()
+    POST_PURCHASE_UPDATE = auto()
+    ACCOUNT_UPDATE = auto()
+    HUMAN_AGENT = auto()
 
 
 class SendResponse(BaseModel):
