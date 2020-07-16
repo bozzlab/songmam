bbcfddaa5b308ee58b6ef136f29c73219fa4eed8
Nutchanon Ninyawee <me@nutchanon.org>
2020-07-16 23:57:34 +0700

    move sendAction

---
+++ songmam/facebook/messaging.py
@@ -0,0 +1,11 @@
+# https://developers.facebook.com/docs/messenger-platform/send-messages/quick-replies
+from enum import auto
+
+from songmam.utils import AutoNameLower
+
+
+class SenderAction(AutoNameLower):
+    """https://developers.facebook.com/docs/messenger-platform/send-messages/sender-actions"""
+    TYPING_ON = auto()
+    TYPING_OFF = auto()
+    MARK_SEEN = auto()
