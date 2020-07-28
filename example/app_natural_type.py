9c7955a9ec3af62e377d832212d973f1d2288bd9
Nutchanon Ninyawee <me@nutchanon.org>
2020-07-28 12:51:49 +0700

    simplify

--- example/app_natural_type.py
+++ example/app_natural_type.py
@@ -1,15 +1,10 @@
 from functools import partial
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
-
-# os.environ['PAGE_ACCESS_TOKEN'] = "MY Access token"
-# os.environ['PAGE_VERIFY_TOKEN'] = "MY Verify token"
-from jinja2 import Environment, select_autoescape, FileSystemLoader
-from loguru import logger
 
 from songmam import Webhook
-from songmam.api.events import MessageEvent, PostBackEvent
+from songmam.api.events import MessageEvent
 from songmam.api.content import ContentButton
 
 from songmam.humanTyping import HumanTyping
@@ -18,9 +13,7 @@ from songmam.page import Page
 
 
 
-page = Page(
-    whitelisted_domains=["https://e1bbfd3fd123.ngrok.io"],
-)
+page = Page()
 app = FastAPI()
 humanTyping =HumanTyping()
 
@@ -29,7 +22,7 @@ page.add_verification_middleware(app)
 
 
 @app.post("/webhook")
-async def handle_entry(webhook: Webhook, request: Request):
+async def handle_entry(webhook: Webhook):
     await page.handle_webhook(webhook)
     return "ok"
 
@@ -41,23 +34,11 @@ async def echo(message: MessageEvent):
     content = ContentButton(
         text=f"replied to {message.text}",
     )
-    # page.send(message.sender, content)
     typing_fn = partial(page.typing_on_sync, message.sender)
     stop_typing_fn = partial(page.typing_off_sync, message.sender)
     await humanTyping.act_typing_simple(message.text, typing_fn, stop_typing_fn)
     page.reply_sync(message, content)
-    # page._send(
-    #
-    #     )
 
-
-@page.handle_postback
-async def log(event: PostBackEvent):
-    logger.info(f"{event.entry} ")
-
-@page.callback(payloads=["test"], quick_reply=True, button=False)
-async def log3(event):
-    logger.info("log3")
 
 if __name__ == "__main__":
     import uvicorn
